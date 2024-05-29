56071137bd2ae138a4f32e33bfb0409ec7924f42
favycodezz <ezeliorafavour81@gmail.com>
2024-05-29 07:25:33 -0700

    almost done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ### Things I learnt during the course of this project
+- **An Immediately Invoked Function Expression (IIFE)** is used to encapsulate the entire code block. This pattern is often used to create a private scope for variables, preventing them from polluting the global scope.
--- script.js
+++ script.js
@@ -1,80 +1,59 @@
-const clear = document.querySelector('.clear');
-// the query selector selects the first frst classname with .class and in our case we have just one
-const dateEl = document.getElementById('date');
-const list = document.getElementById('list');
-const input = document.getElementById('input');
-let LIST = [];
-const check = "ri-checkbox-circle-fill";
-const unCheck = "ri-add-circle-line";
-const lineThrough = 'line-through';
+(() => {
+  let toDoListArray = [];
 
-function addToDo(toDo,id,done,trash){
+  const form = document.querySelector(".form");
+  const input = form.querySelector(".form_input");
+  const ul = document.querySelector(".toDoList");
 
-    if (trash){
-        return;
-    } 
+  // Add event listeners
+  form.addEventListener("submit", (e) => {
+    // Prevent browser from default behaviour - page reload
+    e.preventDefault();
 
-    const Done = done? check : unCheck;
-    const Line = done? lineThrough : "";
+    // Give item a unique ID
+    let itemID = String(Date.now());
+    // Get or assign input value
+    let todoItem = input.value;
+    // Pass ID and item into functions
+    addItemToDOM(itemID, todoItem);
+    addItemToArray(itemID, todoItem);
+    // Clear the input box, this is default behaviour but we removed it
+    input.value = "";
+  });
 
-    const text = `
-                <li class="list">
-                    <i class="ri-checkbox-blank-circle-line ${Done}" job="complete" id ="${id}"></i>
-                    <p class="text ${Line}">${toDo}</p>
-                    <i class="ri-delete-bin-4-line" job="delete" id ="${id}"></i>
-                </li>
-             `
-    const position = "beforeend";
-    list.insertAdjacentHTML(position, text);
-}
-addToDo("drink coffe");
+  ul.addEventListener("click", (e) => {
+    let id = e.target.getAttribute("data-id");
+    if (!id) return; // User clicked on something else
+    removeItemFromDOM(id);
+    removeItemFromArray(id);
+  });
 
-// whenever the user press a key, it fires up a function
-document.addEventListener("keyup", function(event){
-    // A keycode is a code that represents a keypress, for enter the keycode =13
-    // if the user selects the enter 
-    if (event.key == 13){
-       const toDo = input.value; 
-       if (toDo) ={
-        addToDo(toDo,id,);
-       }
-       input.value = ""; 
-    }
-});
+  // Functions
+  function addItemToDOM(itemID, todoItem) {
+    // Create an li
+    const li = document.createElement("li");
+    li.setAttribute("data-id", itemID);
+    // Add todo item next to li
+    li.innerText = todoItem;
+    // Add li to the DOM
+    ul.appendChild(li);
+  }
 
-function completeToDo(){
-    element.classList.toggle(check);
-    element.classList.toggle(unCheck);
-}
+  function addItemToArray(itemID, todoItem) {
+    // Add item to array as an object with an ID so we can find and delete it later
+    toDoListArray.push({ itemID, todoItem });
+    console.log(toDoListArray);
+  }
 
+  function removeItemFromDOM(id) {
+    var li = document.querySelector('[data-id="' + id + '"]');
+    // Remove list item
+    ul.removeChild(li);
+  }
 
-// const addForm = document.querySelector('.add');
-// const list = document.querySelector('.todos');
-// const message = document.getElementById('warning')
-
-// addForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const todo = addForm.add.value.trim();
-
-//     if(todo.length != 0){
-//         generateTemplate(todo);
-//         addForm.reset();
-//     }else{
-//         // alert('Please input task');
-//         message.innerHTML = 'Please input task'
-//     }
-// });
-
-// const generateTemplate = (todo) => {
-//     const html = `<li class='list-group-item d-flex justify-content-between align-items-center'>
-//         <span>${todo}</span>
-//         <i class="fa fa-trash-o delete"></i>
-//     </li>`;
-//     list.innerHTML += html;
-// }
-
-// list.addEventListener('click', (e) => {
-//     if(e.target.classList.contains('delete')) {
-//         e.target.parentElement.remove();
-//     }
-// })
+  function removeItemFromArray(id) {
+    // Create a new todolist array with all the li's that don't match the ID
+    toDoListArray = toDoListArray.filter((item) => item.itemID !== id);
+    console.log(toDoListArray);
+  }
+})();
